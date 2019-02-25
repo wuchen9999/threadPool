44674e1fc3d456e03d195eba84e7e4b579513a1d
wuchen9999 <453821963@qq.com>
2019-02-25 22:08:37 +0800

    add

---
+++ src/main/java/com/qy/TheadPool/poolDemo1.java
@@ -0,0 +1,10 @@
+package com.qy.TheadPool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class poolDemo1 {
+
+	public static void main(String[] args) {
+		ThreadPoolExecutor dd =  new ThreadPoolExecutor(0, 0, 0, null, null);
+	}
+}
